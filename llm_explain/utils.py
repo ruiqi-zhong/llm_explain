e474b6ffb21c3f30a05bec615fe27858b3188a81
Ruiqi Zhong <ruiqi-zhong@scf.berkeley.edu>
2025-05-07 17:55:24 -0700

    adding utils

--- llm_explain/utils.py
+++ llm_explain/utils.py
@@ -23,11 +23,11 @@ logger.addHandler(console_handler)
 logger.debug("Logger initialized")
 
 
-def extract_tag_from_text(text: str, tag: str) -> str | None:
+def extract_tag_from_output(output: str, tag: str) -> str | None:
     try:
-        return re.search(rf"<{tag}>(.*?)</{tag}>", text).group(1)
+        return re.search(rf"<{tag}>(.*?)</{tag}>", output).group(1)
     except Exception as e:
-        logger.warning(f"Error extracting tag {tag} from text {text}: {e}")
+        logger.warning(f"Error extracting tag {tag} from output {output}: {e}")
         return None
     
     
